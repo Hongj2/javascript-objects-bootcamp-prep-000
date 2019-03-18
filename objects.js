9f007d3e88036e1f044fae111d854f832f217e7c
Jessica Hong <jessicahong.24@gmail.com>
2019-03-18 15:11:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 }
 
 function updatePlaylist (playlist, artistName, songTitle){
-  playlist[Phil Ochs] = ["Here\s to the State of Mississippi"]
+  playlist.philOchs = `Here\s to the State of Mississippi`;
   return playlist
 }
 function removeFromPlaylist (playlist,artistName){
